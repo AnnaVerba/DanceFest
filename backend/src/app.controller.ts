35879c1e494d3c418256cd957513e64f92e08a9e
Anna Verba <Anna.Verba@fivesysdev.com>
2026-07-10 15:26:04 +0300

    test: add bugs

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import process from 'process';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,20 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('health')
+  async healthCheck(): Promise<string> {
+    this.appService.getHello;
+
+    return 'OK';
+  }
+
+  @Get('variables')
+  variables() {
+    return {
+      status: 'OK',
+      env: process.env,
+    };
   }
 }
